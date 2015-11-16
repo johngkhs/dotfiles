12259af95f331aa839ac0f52a5ea54b4240210c9
jkaczor <jkaczor@quicinc.com>
2015-11-15 20:24:03 -0600

    Added script to create usr folder in home directory

--- install-all.sh
+++ install-all.sh
@@ -3,6 +3,7 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+./install-home-dir.sh
 ./install-i3.sh
 ./install-tmux.sh
 ./install-zshrc.sh
---
+++ install-home-dir.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o xtrace
+
+mkdir -p "$HOME/usr/bin"
+mkdir -p "$HOME/usr/lib"
+mkdir -p "$HOME/usr/include"
