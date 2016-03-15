25add52776b100421e0fcc2e0124cd1699f0e757
jkaczor <jkaczor@quicinc.com>
2016-03-14 19:11:53 -0500

    Fix mistype, remove unused line

--- powerline/setup_powerline.sh
+++ powerline/setup_powerline.sh
@@ -13,8 +13,7 @@ git clone https://github.com/jaspernbrouwer/powerline-gitstatus /tmp/powerline_g
 cd /tmp/powerline_gitstatus_src && python setup.py install --home="$HOME/usr"
 
 echo '#!/usr/bin/env bash' > "$script_dir/source_powerline.zsh"
-powerline_basepath=$(pip show "powerline-status" | grep "Location:" | awk '{print $2}')
-echo "source $(find $HOME/usr/lib/python --name powerline.zsh -print -quit)" >> "$script_dir/source_powerline.zsh"
+echo "source $(find $HOME/usr/lib/python -name powerline.zsh -print -quit)" >> "$script_dir/source_powerline.zsh"
 chmod +x "$script_dir/source_powerline.zsh"
 mkdir -p "$HOME/.config"
 ln -s "$script_dir" "$HOME/.config/powerline"
