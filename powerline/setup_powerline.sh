bf3089d3d95236448bdd53a0911064c7dc7a5c7c
johngkhs <johngkhs@gmail.com>
2016-03-14 19:16:19 -0500

    Clean up after building

--- powerline/setup_powerline.sh
+++ powerline/setup_powerline.sh
@@ -8,9 +8,11 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 git clone https://github.com/powerline/powerline /tmp/powerline_src
 cd /tmp/powerline_src && python setup.py install --home="$HOME/usr"
 cp /tmp/powerline_src/scripts/powerline "$HOME/usr/bin"
+rm -rf /tmp/powerline_src
 
 git clone https://github.com/jaspernbrouwer/powerline-gitstatus /tmp/powerline_gitstatus_src
 cd /tmp/powerline_gitstatus_src && python setup.py install --home="$HOME/usr"
+rm -rf /tmp/powerline_gitstatus_src
 
 echo '#!/usr/bin/env bash' > "$script_dir/source_powerline.zsh"
 echo "source $(find $HOME/usr/lib/python -name powerline.zsh -print -quit)" >> "$script_dir/source_powerline.zsh"
