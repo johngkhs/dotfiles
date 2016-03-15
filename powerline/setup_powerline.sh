9fd2f3085b4567d5bed5d7c745c5c6fc90750f96
jkaczor <jkaczor@quicinc.com>
2016-03-14 19:03:03 -0500

    Setup powerline scripts to home directory

--- powerline/setup_powerline.sh
+++ powerline/setup_powerline.sh
@@ -5,11 +5,16 @@ set -o xtrace
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-pip install "powerline-status"
-pip install "powerline-gitstatus"
+git clone https://github.com/powerline/powerline /tmp/powerline_src
+cd /tmp/powerline_src && python setup.py install --home="$HOME/usr"
+cp /tmp/powerline_src/scripts/powerline "$HOME/usr/bin"
+
+git clone https://github.com/jaspernbrouwer/powerline-gitstatus /tmp/powerline_gitstatus_src
+cd /tmp/powerline_gitstatus_src && python setup.py install --home="$HOME/usr"
+
 echo '#!/usr/bin/env bash' > "$script_dir/source_powerline.zsh"
 powerline_basepath=$(pip show "powerline-status" | grep "Location:" | awk '{print $2}')
-echo "source $powerline_basepath/powerline/bindings/zsh/powerline.zsh" >> "$script_dir/source_powerline.zsh"
+echo "source $(find $HOME/usr/lib/python --name powerline.zsh -print -quit)" >> "$script_dir/source_powerline.zsh"
 chmod +x "$script_dir/source_powerline.zsh"
 mkdir -p "$HOME/.config"
 ln -s "$script_dir" "$HOME/.config/powerline"
