d39d25656037f12a5ddda741d3534e228e307e25
John <johngkhs@gmail.com>
2016-08-03 18:12:07 -0500

    Remove unused line

--- git/setup_git.sh
+++ git/setup_git.sh
@@ -5,6 +5,4 @@ set -o xtrace
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-git clone https://github.com/VundleVim/Vundle.vim "$script_dir/dotvim/bundle/Vundle.vim"
-
 ln -s "$script_dir/gitconfig" "$HOME/.gitconfig"
