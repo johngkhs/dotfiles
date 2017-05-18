f8503e02392c89308d9adcb4d066e4ffcf605b81
John <johngkhs@gmail.com>
2017-05-18 18:00:41 -0500

    Remove unnecessary code, use fast syntax highlighting and dircolors

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,14 +3,14 @@ set -o errexit -o nounset -o xtrace
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 git clone https://github.com/zsh-users/zsh-history-substring-search "$script_dir/dotzsh/plugins/zsh-history-substring-search"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting "$script_dir/dotzsh/plugins/zsh-syntax-highlighting"
-git clone https://github.com/rupa/z "$script_dir/dotzsh/plugins/z"
+git clone https://github.com/zdharma/fast-syntax-highlighting.git "$script_dir/dotzsh/plugins/fast-syntax-highlighting"
+git clone https://github.com/seebi/dircolors-solarized.git "$script_dir/dotzsh/dircolors-solarized"
 git clone https://github.com/clvv/fasd /tmp/fasd
 cp /tmp/fasd/fasd "$HOME/usr/bin"
 git clone https://github.com/wellle/tmux-complete.vim /tmp/tmux-complete
 cp /tmp/tmux-complete/sh/tmuxcomplete "$HOME/usr/bin"
 git clone https://github.com/robbyrussell/oh-my-zsh.git /tmp/oh-my-zsh
-cp -R /tmp/oh-my-zsh/plugins/extract "$script_dir/dotzsh/plugins/zsh-extract"
+cp -R /tmp/oh-my-zsh/plugins/extract "$script_dir/dotzsh/plugins/extract"
 
 cp -n "$script_dir/zshrc.shared" "$script_dir/zshrc"
 
