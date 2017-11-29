5de067a854decb0b5cac5167cc3550068ea7b47d
John <johngkhs@gmail.com>
2017-11-28 21:21:48 -0600

    Enforce python not python3 for powerline for speed purposes

--- powerline/install.sh
+++ powerline/install.sh
@@ -2,21 +2,15 @@
 set -o errexit -o nounset -o xtrace
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if command -v python3 >/dev/null 2>&1; then
-    python_binary='python3'
-else
-    python_binary='python'
-fi
 export PYTHONPATH="$HOME/usr/lib/python:${PYTHONPATH-}"
-
 [[ -d /tmp/powerline ]] || git clone https://github.com/powerline/powerline /tmp/powerline
 cd /tmp/powerline
-$python_binary setup.py install --home="$HOME/usr"
+python setup.py install --home="$HOME/usr"
 cp -n /tmp/powerline/scripts/powerline "$HOME/usr/bin"
 
 [[ -d /tmp/powerline_gitstatus ]] || git clone https://github.com/jaspernbrouwer/powerline-gitstatus /tmp/powerline_gitstatus
 cd /tmp/powerline_gitstatus
-$python_binary setup.py install --home="$HOME/usr"
+python setup.py install --home="$HOME/usr"
 
 echo '#!/usr/bin/env bash' > "/tmp/source_powerline.zsh"
 echo "source $(find $HOME/usr/lib/python -name powerline.zsh -print -quit)" >> "/tmp/source_powerline.zsh"
