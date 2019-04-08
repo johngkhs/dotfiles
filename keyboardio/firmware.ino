446ba1827a67df90f8960ff9a946e3763cf685f3
John <johngkhs@gmail.com>
2019-04-07 21:49:56 -0700

    Move enter key to main thumb cluster

--- keyboardio/firmware.ino
+++ keyboardio/firmware.ino
@@ -8,7 +8,7 @@
 #include "Kaleidoscope-Escape-OneShot.h"
 
 enum { M_FN_LED, M_FN_ANY, M_FN_EQUALS,
-       M_PAGEUP, M_FN_PAGEUP, M_FN_A, M_FN_F, M_TAB, M_FN_TAB, M_FN_ENTER, M_ENTER, M_FN_SEMICOLON, M_FN_APOSTROPHE,
+       M_PAGEUP, M_FN_PAGEUP, M_FN_S, M_FN_F, M_TAB, M_FN_TAB, M_FN_ENTER, M_ENTER, M_FN_SEMICOLON, M_FN_APOSTROPHE,
        M_ESCAPE, M_FN_ESCAPE, M_FN_BUTTERFLY, M_BUTTERFLY, M_FN_COMMA, M_FN_PERIOD };
 enum { QWERTY, NUMPAD, FUNCTION };
 
@@ -26,7 +26,7 @@ KEYMAPS(
    Key_RightParen,        Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
                           Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    Key_RightBracket,      Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
-   Key_Escape, OSM(RightShift), Key_Spacebar, OSM(RightAlt),
+   Key_Enter, OSM(RightShift), Key_Spacebar, OSM(RightAlt),
    ShiftToLayer(FUNCTION)),
 
   [NUMPAD] =  KEYMAP_STACKED
@@ -37,17 +37,17 @@ KEYMAPS(
    ___, ___, ___, ___,
    ___,
 
-   ___,  ___,         ___,         ___,         ___,         ___,           ___,
-   ___,  ___,         Key_Keypad7, Key_Keypad8, Key_Keypad9, Key_KeypadDot, ___,
-         Key_Keypad0, Key_Keypad4, Key_Keypad5, Key_Keypad6, Key_KeypadMultiply, Key_KeypadAdd,
-   ___,  ___,         Key_Keypad1, Key_Keypad2, Key_Keypad3, Key_KeypadDivide,   Key_KeypadSubtract,
+   ___, ___,         ___,         ___,         ___,         ___,                ___,
+   ___, ___,         Key_Keypad7, Key_Keypad8, Key_Keypad9, Key_KeypadDot,      ___,
+        Key_Keypad0, Key_Keypad4, Key_Keypad5, Key_Keypad6, Key_KeypadMultiply, Key_KeypadAdd,
+   ___, ___,         Key_Keypad1, Key_Keypad2, Key_Keypad3, Key_KeypadDivide,   Key_KeypadSubtract,
    ___, ___, ___, ___,
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
   (___,            Key_F1,          Key_F2,     Key_F3, Key_F4,    Key_F5, M(M_FN_LED),
    ___,            ___,             ___,        ___,    ___,       ___,    M(M_FN_TAB),
-   M(M_FN_PAGEUP), M(M_FN_A),       ___,        ___,    M(M_FN_F), ___,
+   M(M_FN_PAGEUP), ___,             M(M_FN_S),  ___,    M(M_FN_F), ___,
    ___,            Key_PrintScreen, Key_Insert, ___,    ___,       ___,    M(M_FN_ESCAPE),
    ___, Key_Delete, ___, ___,
    ___,
@@ -56,7 +56,7 @@ KEYMAPS(
    M(M_FN_ENTER),     Consumer_PlaySlashPause, Consumer_VolumeDecrement,   Consumer_VolumeIncrement, Consumer_ScanPreviousTrack, Consumer_ScanNextTrack, M(M_FN_EQUALS),
                       Key_LeftArrow,           Key_DownArrow,              Key_UpArrow,              Key_RightArrow,             M(M_FN_SEMICOLON),      M(M_FN_APOSTROPHE),
    M(M_FN_BUTTERFLY), Consumer_Mute,           ___,                        M(M_FN_COMMA),            M(M_FN_PERIOD),             Key_Backslash,          Key_Pipe,
-   ___, ___, Key_Enter, ___,
+   ___, ___, Key_Escape, ___,
    ___)
 
   )
@@ -77,7 +77,7 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
     case M_FN_EQUALS: return MACRODOWN(T(Equals), T(Equals));
     case M_FN_COMMA: return MACRODOWN(D(LeftShift), T(Comma), U(LeftShift), D(LeftShift), T(Period), U(LeftShift), T(LeftArrow));
     case M_FN_PERIOD: return MACRODOWN(T(Minus), D(LeftShift), T(Period), U(LeftShift));
-    case M_FN_A: return MACRODOWN(D(LeftControl), T(LeftArrow), U(LeftControl));
+    case M_FN_S: return MACRODOWN(D(LeftControl), T(LeftArrow), U(LeftControl));
     case M_FN_F: return MACRODOWN(D(LeftControl), T(RightArrow), U(LeftControl));
     case M_FN_TAB: return MACRODOWN(D(LeftShift), T(9), U(LeftShift), D(LeftShift), T(0), U(LeftShift), T(LeftArrow));
     case M_FN_ENTER: return MACRODOWN(D(LeftShift), T(9), U(LeftShift), D(LeftShift), T(0), U(LeftShift));
