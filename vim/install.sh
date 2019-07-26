5e43855fa95190946b6023e8565de0c4776ab8ca
John Kaczor <johngk@google.com>
2019-07-26 15:12:56 -0700

    Add ideavimrc

--- vim/install.sh
+++ vim/install.sh
@@ -4,3 +4,4 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 [[ -d "$HOME/.vim/bundle/Vundle.vim" ]] || git clone https://github.com/VundleVim/Vundle.vim "$HOME/.vim/bundle/Vundle.vim"
 cp "$script_dir/vimrc" "$HOME/.vimrc"
+cp "$script_dir/ideavimrc" "$HOME/.ideavimrc"
