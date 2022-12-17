3e3e944e0b837f528f27859b572f1a4b918b2e76
John Kaczor <jkaczor@netflix.com>
2022-12-16 19:01:15 -0700

    Convert to single quotes

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,18 +2,18 @@
 --                                                paq                                                         --
 ----------------------------------------------------------------------------------------------------------------
 
-local PKGS = {
-  "savq/paq-nvim",
-  "alexghergh/nvim-tmux-navigation",
-  "navarasu/onedark.nvim",
-  "tpope/vim-repeat",
-  "ggandor/flit.nvim",
-  "ggandor/leap.nvim",
-  "ibhagwan/fzf-lua",
-  "terrortylor/nvim-comment",
-  "PeterRincker/vim-argumentative",
-  "derekwyatt/vim-fswitch",
-  "neovim/nvim-lspconfig",
+local PACKAGES = {
+  'savq/paq-nvim',
+  'alexghergh/nvim-tmux-navigation',
+  'navarasu/onedark.nvim',
+  'tpope/vim-repeat',
+  'ggandor/flit.nvim',
+  'ggandor/leap.nvim',
+  'ibhagwan/fzf-lua',
+  'terrortylor/nvim-comment',
+  'PeterRincker/vim-argumentative',
+  'derekwyatt/vim-fswitch',
+  'neovim/nvim-lspconfig',
   'hrsh7th/nvim-cmp',
   'hrsh7th/cmp-nvim-lsp',
   'ojroques/nvim-hardline',
@@ -23,17 +23,23 @@ local PKGS = {
   'lewis6991/gitsigns.nvim',
 }
 
+local paq = require('paq')
+
 local function bootstrap_paq()
     local path = vim.fn.stdpath 'data' .. '/site/pack/paqs/start/paq-nvim'
     if vim.fn.empty(vim.fn.glob(path)) > 0 then
         vim.fn.system { 'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', path, }
         vim.cmd('packadd paq-nvim')
-        require("paq")(PKGS).install()
+        paq(PACKAGES).install()
     end
 end
 
 bootstrap_paq()
-require("paq")(PKGS)
+paq(PACKAGES)
+
+----------------------------------------------------------------------------------------------------------------
+--                                             functions                                                      --
+----------------------------------------------------------------------------------------------------------------
 
 local function map(mode, lhs, rhs, opts)
   local options = {noremap = true}
@@ -41,7 +47,27 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-vim.g.mapleader = " "
+----------------------------------------------------------------------------------------------------------------
+--                                          general settings                                                  --
+----------------------------------------------------------------------------------------------------------------
+
+vim.g.mapleader = ' '
+vim.api.nvim_set_option('clipboard', 'unnamedplus')
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.cmd('set nohlsearch')
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.wildignore = '*.o,*.a,*/bin/*'
+vim.opt.grepprg = 'rg --vimgrep --no-heading --smart-case --column --line-number'
+
+require('onedark').setup {
+  transparent = true
+}
+require('onedark').load()
+
 
 ----------------------------------------------------------------------------------------------------------------
 --                                             hardline                                                       --
@@ -57,32 +83,32 @@ require('illuminate').configure {
 
 require('gitsigns').setup()
 
-map("n", "<leader><leader>r", '<cmd>lua require("renamer").rename()<cr>', {silent = true})
-map("v", "<leader><leader>r", '<cmd>lua require("renamer").rename()<cr>', {silent = true})
+map('n', '<leader><leader>r', '<cmd>lua require("renamer").rename()<cr>', {silent = true})
+map('v', '<leader><leader>r', '<cmd>lua require("renamer").rename()<cr>', {silent = true})
 
--- require("nvim-web-devicons").setup {}
--- require("trouble").setup {
+-- require('nvim-web-devicons').setup {}
+-- require('trouble').setup {
 --   height = 40,
 --   action_keys = {
---     jump = "<tab>",
---     jump_close = "<cr>",
+--     jump = '<tab>',
+--     jump_close = '<cr>',
 --     hover = {},
 --   },
 -- }
 --
--- map("n", "<leader>a", "<cmd>TroubleToggle quickfix<cr>")
--- map("n", "<leader>e", "<cmd>TroubleToggle document_diagnostics<cr>")
--- map("n", "gr", "<cmd>Trouble lsp_references<cr>")
+-- map('n', '<leader>a', '<cmd>TroubleToggle quickfix<cr>')
+-- map('n', '<leader>e', '<cmd>TroubleToggle document_diagnostics<cr>')
+-- map('n', 'gr', '<cmd>Trouble lsp_references<cr>')
 ----------------------------------------------------------------------------------------------------------------
 --                                        nvim-tmux-navigation                                                --
 ----------------------------------------------------------------------------------------------------------------
 
 require('nvim-tmux-navigation')
 
-map("n", "<c-h>", "<cmd>NvimTmuxNavigateLeft<cr>", {silent = true})
-map("n", "<c-j>", "<cmd>NvimTmuxNavigateDown<cr>", {silent = true})
-map("n", "<c-k>", "<cmd>NvimTmuxNavigateUp<cr>", {silent = true})
-map("n", "<c-l>", "<cmd>NvimTmuxNavigateRight<cr>", {silent = true})
+map('n', '<c-h>', '<cmd>NvimTmuxNavigateLeft<cr>', {silent = true})
+map('n', '<c-j>', '<cmd>NvimTmuxNavigateDown<cr>', {silent = true})
+map('n', '<c-k>', '<cmd>NvimTmuxNavigateUp<cr>', {silent = true})
+map('n', '<c-l>', '<cmd>NvimTmuxNavigateRight<cr>', {silent = true})
 
 ----------------------------------------------------------------------------------------------------------------
 --                                              fzf-lua                                                       --
@@ -93,25 +119,25 @@ require('fzf-lua').setup {
     fullscreen = true,
     preview = {
       layout = 'vertical',
-      vertical = "up:45%",
+      vertical = 'up:45%',
     }
   }
 }
 
-map("n", "<enter>", "<cmd>lua vim.lsp.buf.definition()<cr>")
-map("n", "<leader>f", "<cmd>FzfLua files<cr>")
-map("n", "<leader>b", "<cmd>FzfLua buffers<cr>")
-map("n", "<leader>s", "<cmd>FzfLua grep_cword<cr>" )
-map("n", "<leader>t", "<cmd>FzfLua grep<cr>" )
-map("n", "gr", "<cmd>FzfLua lsp_references<cr>")
--- map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
--- map("n", "gR", "<cmd>lua vim.lsp.buf.references()<cr>")
+map('n', '<enter>', '<cmd>lua vim.lsp.buf.definition()<cr>')
+map('n', '<leader>f', '<cmd>FzfLua files<cr>')
+map('n', '<leader>b', '<cmd>FzfLua buffers<cr>')
+map('n', '<leader>s', '<cmd>FzfLua grep_cword<cr>' )
+map('n', '<leader>t', '<cmd>FzfLua grep<cr>' )
+map('n', 'gr', '<cmd>FzfLua lsp_references<cr>')
+-- map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+-- map('n', 'gR', '<cmd>lua vim.lsp.buf.references()<cr>')
 
 ----------------------------------------------------------------------------------------------------------------
 --                                          lspconfig and nvim-cmp                                            --
 ----------------------------------------------------------------------------------------------------------------
 
-local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 local servers = { 'pyright', 'clangd' }
 local lspconfig = require('lspconfig')
 for _, lsp in ipairs(servers) do
@@ -153,77 +179,57 @@ cmp.setup {
 ----------------------------------------------------------------------------------------------------------------
 
 require('nvim_comment').setup({create_mappings = false})
-map("n", "<leader>,", ":CommentToggle<cr>")
-map("v", "<leader>,", ":'<,'>CommentToggle<cr>")
+map('n', '<leader>,', ':CommentToggle<cr>')
+map('v', '<leader>,', ':\'<,\'>CommentToggle<cr>')
 
 ----------------------------------------------------------------------------------------------------------------
 --                                           flit and leap                                                    --
 ----------------------------------------------------------------------------------------------------------------
 
 require('leap').add_default_mappings()
-require("flit").setup {}
+require('flit').setup {}
 
 ----------------------------------------------------------------------------------------------------------------
 --                                            vim-fswitch                                                     --
 ----------------------------------------------------------------------------------------------------------------
 
-map("n", "<leader>h", "<cmd>FSHere<cr>")
-
-----------------------------------------------------------------------------------------------------------------
---                                          general settings                                                  --
-----------------------------------------------------------------------------------------------------------------
-
-vim.api.nvim_set_option("clipboard", "unnamedplus")
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.cmd("set nohlsearch")
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.wildignore = '*.o,*.a,*/bin/*'
-vim.opt.grepprg = 'rg --vimgrep --no-heading --smart-case --column --line-number'
-
-require('onedark').setup {
-  transparent = true
-}
-require('onedark').load()
+map('n', '<leader>h', '<cmd>FSHere<cr>')
 
 ----------------------------------------------------------------------------------------------------------------
 --                                          general mappings                                                  --
 ----------------------------------------------------------------------------------------------------------------
 
-map("v", "<tab>", ">gv")
-map("v", "<s-tab>", "<gv")
-map("n", "<tab>", ">>")
-map("n", "<s-tab>", "<<")
+map('v', '<tab>', '>gv')
+map('v', '<s-tab>', '<gv')
+map('n', '<tab>', '>>')
+map('n', '<s-tab>', '<<')
 
-map("i", "jk", "<esc>")
-map("c", "jk", "<c-c>")
-map("i", "jj", "()<esc>i")
-map("i", "kk", "()")
-map("i", "jl", "<esc>A<space>{<cr>}<esc><up>A<cr>")
+map('i', 'jk', '<esc>')
+map('c', 'jk', '<c-c>')
+map('i', 'jj', '()<esc>i')
+map('i', 'kk', '()')
+map('i', 'jl', '<esc>A<space>{<cr>}<esc><up>A<cr>')
 
-map("n", "J", "20j")
-map("n", "K", "20k")
-map("v", "J", "20j")
-map("v", "K", "20k")
+map('n', 'J', '20j')
+map('n', 'K', '20k')
+map('v', 'J', '20j')
+map('v', 'K', '20k')
 
-map("n", "<leader>w", "<cmd>w<cr>")
-map("n", "<leader><leader>w", "<cmd>wa<cr>")
-map("n", "<leader>q", "<cmd>q<cr>")
-map("n", "<leader><leader>q", "<cmd>qa!<cr>")
+map('n', '<leader>w', '<cmd>w<cr>')
+map('n', '<leader><leader>w', '<cmd>wa<cr>')
+map('n', '<leader>q', '<cmd>q<cr>')
+map('n', '<leader><leader>q', '<cmd>qa!<cr>')
 
-map("n", "H", "<c-o>")
-map("n", "L", "<c-i>")
+map('n', 'H', '<c-o>')
+map('n', 'L', '<c-i>')
 
-map("n", "<leader>r", ":%s@<c-r><c-w>@@gc<left><left><left>")
-map("v", "<leader>r", "y:%s@<C-r>\"@@gc<left><left><left>")
+map('n', '<leader>r', ':%s@<c-r><c-w>@@gc<left><left><left>')
+map('v', '<leader>r', 'y:%s@<C-r>\"@@gc<left><left><left>')
 
-map("n", "<leader>R", ":%s@<c-r><c-w>@@g<left><left>")
-map("v", "<leader>R", "y:%s@<C-r>\"@@g<left><left>")
+map('n', '<leader>R', ':%s@<c-r><c-w>@@g<left><left>')
+map('v', '<leader>R', 'y:%s@<C-r>\"@@g<left><left>')
 
-map("n", "<leader>j", "mzJ`z")
+map('n', '<leader>j', 'mzJ`z')
 
-map("n", "<leader>E", ":vsplit $MYVIMRC<cr>")
-map("n", "<leader>S", ":source $MYVIMRC<cr>")
+map('n', '<leader>E', ':vsplit $MYVIMRC<cr>')
+map('n', '<leader>S', ':source $MYVIMRC<cr>')
