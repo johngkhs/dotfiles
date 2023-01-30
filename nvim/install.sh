a7ec77a8e3d0918f8d06474bcad3a2fe6062ce55
John Kaczor <jkaczor@hhb-l-gmdev08.ntglobalmarkets.com>
2023-01-30 14:49:43 -0600

    Fix missing quote

--- nvim/install.sh
+++ nvim/install.sh
@@ -2,5 +2,5 @@
 set -o errexit -o nounset
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mkdir -p "$HOME/.config/nvim
+mkdir -p "$HOME/.config/nvim"
 cp "$script_dir/init.lua" "$HOME/.config/nvim/init.lua"
