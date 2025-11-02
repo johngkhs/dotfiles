3b654fb3f1917992885e8c96c04b44bf7189c9d7
John <johngkhs.gmail.com>
2025-11-01 19:51:36 -0600

    Additional jj configs

--- git/install.sh
+++ git/install.sh
@@ -2,4 +2,7 @@
 set -o errexit -o nounset
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+mkdir -p "$HOME/.config/jj"
+cp "$script_dir/jj/config.toml" "$HOME/.config/jj/config.toml"
+
 cp "$script_dir/gitconfig" "$HOME/.gitconfig"
