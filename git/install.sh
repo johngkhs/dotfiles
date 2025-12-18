43890436bab1d8e04bcc767d3c0d6203d33c10b7
John <johngkhs.gmail.com>
2025-12-18 16:05:47 -0700

    Improve robustness of interactive rebase script

--- git/install.sh
+++ git/install.sh
@@ -4,5 +4,6 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p "$HOME/.config/jj"
 cp "$script_dir/jj/config.toml" "$HOME/.config/jj/config.toml"
-
+cp "$script_dir/jj/cb" "$HOME/usr/bin"
+cp "$script_dir/jj/ir" "$HOME/usr/bin"
 cp "$script_dir/gitconfig" "$HOME/.gitconfig"
